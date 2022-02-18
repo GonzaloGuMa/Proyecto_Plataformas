5ce253c5b2a9f5d624306c9068d6386944df8dea
Gonzalo <gonzalo.ie.ucr@gmail.com>
2022-02-18 14:53:16 -0600

    Se implementa el fondo con imagenes de internet, se crea los directorios de graficos y de fuentes

--- juego.py
+++ juego.py
@@ -9,6 +9,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))  # (width, height)
 pygame.display.set_caption('PROJECT GAME')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('fonts/KAISG___.TTF', 50)
+
+# Importar imagenes para el fondo
+city_background = pygame.image.load('graphics/background')
+ground_surface = pygame.image.load('graphics/floor.png')
+text_surface = test_font.render('Sobrevive...', False, 'Red')  # (texto, AA, color) # noqa
+
 
 while True:
     # Se utiliza para cerrar el juego
@@ -16,7 +23,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
+    # blit poner una superficie en otra
+    screen.blit(city_background, (0, 0))
+    screen.blit(ground_surface, (0, 50))
+    screen.blit(text_surface, (300, 50))
+
     # actualiza la ventana creada con el obj screen
     pygame.display.update()
     # Frame rate del juego, max frame rate
