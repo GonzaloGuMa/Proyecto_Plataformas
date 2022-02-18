1a1831b14023322ad49fe101540f05f710f2bb35
Gonzalo <gonzalo.ie.ucr@gmail.com>
2022-02-18 14:06:50 -0600

    Se crea la ventana principal del juego, junto al directorio de gráficos a utilizarse

--- juego.py
+++ juego.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 
-print('Hola mundo!')
-print('Prueba diff')
+import pygame
+from sys import exit
+# Se utiliza para que no genere un error al cerrar
+
+# Inicia pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 400))  # (width, height)
+pygame.display.set_caption('PROJECT GAME')
+clock = pygame.time.Clock()
+
+while True:
+    # Se utiliza para cerrar el juego
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # actualiza la ventana creada con el obj screen
+    pygame.display.update()
+    # Frame rate del juego, max frame rate
+    clock.tick(60)
