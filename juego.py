16ecab5f88a108a4d8e7987cf5b1354b05487967
Gonzalo <gonzalo.ie.ucr@gmail.com>
2022-02-18 19:07:15 -0600

    Se agrega imágenes de los personajes a utilizar en el juego, se añade movimiento de los obstáculos en el juego

--- juego.py
+++ juego.py
@@ -12,10 +12,20 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/KAISG___.TTF', 50)
 
 # Importar imagenes para el fondo
-city_background = pygame.image.load('graphics/background')
-ground_surface = pygame.image.load('graphics/floor.png')
-text_surface = test_font.render('Sobrevive...', False, 'Red')  # (texto, AA, color) # noqa
+city_background = pygame.image.load('graphics/background').convert_alpha()
+city = pygame.transform.scale(city_background, (800, 400))
 
+ground_surface = pygame.image.load('graphics/floor.png').convert_alpha()
+ground = pygame.transform.scale(ground_surface, (800, 400))
+
+text_surface = test_font.render('Sobrevive...', True, 'Red')  # (texto, AA, color) # noqa
+
+reiner_texture = pygame.image.load('graphics/ReinerRunning.png').convert_alpha()  # noqa
+reiner = pygame.transform.scale(reiner_texture, (200, 200))
+reiner_x_pos = 600
+
+player_surface = pygame.image.load('graphics/eren_running.png').convert_alpha()
+eren = pygame.transform.scale(player_surface, (75, 75))
 
 while True:
     # Se utiliza para cerrar el juego
@@ -24,9 +34,16 @@ while True:
             pygame.quit()
             exit()
     # blit poner una superficie en otra
-    screen.blit(city_background, (0, 0))
-    screen.blit(ground_surface, (0, 50))
+    screen.blit(city, (0, 0))
+    screen.blit(ground, (0, 50))
     screen.blit(text_surface, (300, 50))
+
+    reiner_x_pos -= 4
+    if reiner_x_pos < -200:
+        reiner_x_pos = 800
+
+    screen.blit(reiner, (reiner_x_pos, 200))
+    screen.blit(eren, (80, 300))
 
     # actualiza la ventana creada con el obj screen
     pygame.display.update()
