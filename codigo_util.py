731c065b2fd0c126c1e879e99f5b9c67402220c1
Gonzalo <gonzalo.ie.ucr@gmail.com>
2022-02-18 22:18:13 -0600

    Se implementa la mecánica de salto y de gravedad para el jugador, se reorganizan los elementos gráficos y se añade un obstáculo más

---
+++ codigo_util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+# if event.type == pygame.MOUSEMOTION:
+#     print(event.pos)
+
+# if event.type == pygame.MOUSEBUTTONDOWN:  # .MOUSEBUTTONUP
+#     print('mouse presionado')
+
+# if event.type == pygame.MOUSEMOTION:
+#     if eren_rect.collidepoint(event.pos):
+#         print('colision!')
+
+
+# if eren_rect.colliderect(reiner_rect):
+#     print('Colisión!!!')
+
+# mouse_pos = pygame.mouse.get_pos()
+# if eren_rect.collidepoint(mouse_pos):
+#     print(pygame.mouse.get_pressed())
+
+
+# pygame.draw.rect(screen, 'gray', score_rect)
+# pygame.draw.rect(screen, 'black', score_rect, 6)
+"""
--- juego.py
+++ juego.py
@@ -6,26 +6,35 @@ from sys import exit
 
 # Inicia pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 400))  # (width, height)
+screen = pygame.display.set_mode((1200, 400))  # (width, height)
 pygame.display.set_caption('PROJECT GAME')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/KAISG___.TTF', 50)
 
 # Importar imagenes para el fondo
-city_background = pygame.image.load('graphics/background').convert_alpha()
-city = pygame.transform.scale(city_background, (800, 400))
+city_background = pygame.image.load('graphics/fondo.jpg').convert_alpha()
+city = pygame.transform.scale(city_background, (1200, 400))
 
 ground_surface = pygame.image.load('graphics/floor.png').convert_alpha()
-ground = pygame.transform.scale(ground_surface, (800, 400))
+ground = pygame.transform.scale(ground_surface, (1200, 400))
 
-text_surface = test_font.render('Sobrevive...', True, 'Red')  # (texto, AA, color) # noqa
+score_surface = test_font.render('Sobrevive...', True, 'Red')  # (texto, AA, color) # noqa
+score_rect = score_surface.get_rect(center=(600, 50))
 
 reiner_texture = pygame.image.load('graphics/ReinerRunning.png').convert_alpha()  # noqa
-reiner = pygame.transform.scale(reiner_texture, (200, 200))
-reiner_x_pos = 600
+reiner_size = pygame.transform.scale(reiner_texture, (300, 300))
+reiner_rect = reiner_size.get_rect(bottomright=(1175, 375))
+
+rubble_texture = pygame.image.load('graphics/rubble.png').convert_alpha()  # noqa
+rubble_size = pygame.transform.scale(rubble_texture, (50, 50))
+rubble_rect = rubble_size.get_rect(bottomright=(80, 375))
 
 player_surface = pygame.image.load('graphics/eren_running.png').convert_alpha()
 eren = pygame.transform.scale(player_surface, (75, 75))
+eren_rect = eren.get_rect(bottomleft=(800, 375))
+
+player_gravity = 0
+
 
 while True:
     # Se utiliza para cerrar el juego
@@ -33,17 +42,35 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        if event.type == pygame.KEYDOWN and eren_rect.bottom >= 375:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -17
+
     # blit poner una superficie en otra
     screen.blit(city, (0, 0))
-    screen.blit(ground, (0, 50))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(ground, (0, 80))
+    screen.blit(score_surface, score_rect)
 
-    reiner_x_pos -= 4
-    if reiner_x_pos < -200:
-        reiner_x_pos = 800
+    # Movimiento de los escombros
+    rubble_rect.x += 4
+    if rubble_rect.right >= 1300:
+        rubble_rect.left = 0
 
-    screen.blit(reiner, (reiner_x_pos, 200))
-    screen.blit(eren, (80, 300))
+    screen.blit(rubble_size, rubble_rect)
+
+    # ***************** Enemigos ***********************
+
+    screen.blit(reiner_size, reiner_rect)
+
+    # ***************** Player *************************
+    # print(eren_rect.left)
+    player_gravity += 1
+    eren_rect.y += player_gravity
+    if eren_rect.bottom >= 375:
+        eren_rect.bottom = 375
+
+    screen.blit(eren, eren_rect)
 
     # actualiza la ventana creada con el obj screen
     pygame.display.update()
